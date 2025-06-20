5e24da48a15954394eb18c7372ab101fd2df02f5
theDovelyDev <143884432+theDovelyDev@users.noreply.github.com>
2025-06-20 18:17:58 -0400

    updated Vistor Counter URL trigger

--- cloudresumechallenge/frontend/js/script.js
+++ cloudresumechallenge/frontend/js/script.js
@@ -7,7 +7,7 @@ function toggleMenu() {
 
 
 // Visitor Counter
-fetch('https://o7vctu4eiznpqfiu4kzc6iaxw40jaczm.lambda-url.us-east-1.on.aws/')
+fetch('https://aqj1luijr1.execute-api.us-east-1.amazonaws.com/dev')
     .then(response => response.json())
     .then(data => {
         document.getElementById("visitCounter").textContent = data["visitorcount"];
