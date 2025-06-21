e4b9d0af9a85ecfbcf1e36d71c6fffbc447345c7
theDovelyDev <143884432+theDovelyDev@users.noreply.github.com>
2025-06-21 14:40:52 -0400

    working on visitor counter

--- cloudresumechallenge/frontend/js/script.js
+++ cloudresumechallenge/frontend/js/script.js
@@ -7,8 +7,8 @@ function toggleMenu() {
 
 
 // Visitor Counter
-fetch('https://aqj1luijr1.execute-api.us-east-1.amazonaws.com/dev')
+function fetch('https://aqj1luijr1.execute-api.us-east-1.amazonaws.com/dev')
     .then(response => response.json())
     .then(data => {
-        document.getElementById("visitCounter").textContent = data["visitorcount"];
+        document.getElementById("visitCounter").textContent = data.body.visitcount;
     })
